5673921ed6ac9781b71ee8d99f0b28c2def0ac7c
kacper <ks300578@student.polsl.pl>
2025-12-06 23:18:46 +0100

    bug-fixes-working-ver

--- main/main.c
+++ main/main.c
@@ -16,18 +16,18 @@ ESP32C3 and MCP2515 based
 
 #include "mcp2515.h"
 
-// SPI PINS
-#  define PIN_NUM_MISO      13
-#  define PIN_NUM_MOSI      12
-#  define PIN_NUM_CLK       11
-#  define PIN_NUM_CS        10
-#  define PIN_NUM_INTERRUPT 4
+// SPI PINS - dummy
+#  define PIN_NUM_MISO      5
+#  define PIN_NUM_MOSI      6
+#  define PIN_NUM_CLK       4
+#  define PIN_NUM_CS        7
+#  define PIN_NUM_INTERRUPT 3
 
 static const char TAG[] = "main";
 
 // RX and TX frames
 CAN_FRAME_t can_frame_rx[1];
-CAN_FRAME can_frame_tx;
+CAN_FRAME_t can_frame_tx[1];
 
 uint16_t counter = 1;
 bool interrupt = false;
@@ -57,10 +57,14 @@ bool SPI_Init(void)
 	// Initialize SPI bus
 	ret = spi_bus_initialize(SPI2_HOST, &bus_cfg, SPI_DMA_CH_AUTO);
 	ESP_ERROR_CHECK(ret);
+	if (ret == ESP_OK)
+		ESP_LOGI(TAG,"SPI INIT OK");
 
 	// Add MCP2515 SPI device to the bus
 	ret = spi_bus_add_device(SPI2_HOST, &dev_cfg, &MCP2515_Object->spi);
 	ESP_ERROR_CHECK(ret);
+	if (ret == ESP_OK)
+			ESP_LOGI(TAG, "MCP INIT OK");
 
 	return true;
 }
@@ -68,21 +72,26 @@ bool SPI_Init(void)
 // Initialize CAN
 void CAN_init(void)
 {
-	MCP2515_init();
+	if (MCP2515_init() == ERROR_OK){
+		ESP_LOGI(TAG, "MCP INIT OK");
+	}
+	else {
+		ESP_LOGE(TAG, "MCP INIT NOK");
+	}
 	SPI_Init();
 	MCP2515_reset();
-	MCP2515_setBitrate(CAN_1000KBPS, MCP_8MHZ);
+	MCP2515_setBitrate(CAN_125KBPS, MCP_8MHZ);
 	MCP2515_setNormalMode();
 }
 
 // Write on CANbus
 void CAN_write(void){
 	// Data
-	can_frame_rx[0]->data[0] = (counter >> 8) & 0xFF;  // MSB
-	can_frame_rx[0]->data[1] = counter & 0xFF;         // LSB
+	can_frame_tx[0]->data[0] = (counter >> 8) & 0xFF;  // MSB
+	can_frame_tx[0]->data[1] = counter & 0xFF;         // LSB
 				
 	// Send data
-	if(MCP2515_sendMessageAfterCtrlCheck(can_frame_rx[0]) != ERROR_OK) {
+	if(MCP2515_sendMessageAfterCtrlCheck(can_frame_tx[0]) != ERROR_OK) {
 		ESP_LOGE(TAG, "Couldn't send message.");
 	} 
 	else {
@@ -98,13 +107,13 @@ void CAN_write(void){
 void CAN_read(void){
 	
 
-	if ((MCP2515_readMessage(RXB0, can_frame_tx) == ERROR_OK) ||
-		(MCP2515_readMessage(RXB1, can_frame_tx) == ERROR_OK)) {
-			printf("CAN ID: 0x%08lX\n", can_frame_tx->can_id);
-			printf("DLC: %d\n", can_frame_tx->can_dlc);
+	if ((MCP2515_readMessage(RXB0, can_frame_rx[0]) == ERROR_OK) ||
+		(MCP2515_readMessage(RXB1, can_frame_rx[0]) == ERROR_OK)) {
+			printf("CAN ID: 0x%08lX\n", can_frame_rx[0]->can_id);
+			printf("DLC: %d\n", can_frame_rx[0]->can_dlc);
 			printf("Data: ");
-			for (int i = 0; i < can_frame_tx->can_dlc; i++) {
-			        printf("%02X ", can_frame_tx->data[i]);
+			for (int i = 0; i < can_frame_rx[0]->can_dlc; i++) {
+			        printf("%02X ", can_frame_rx[0]->data[i]);
 			}
 			printf("\n");
 	}
@@ -121,12 +130,12 @@ void app_main(void)
 	CAN_init();
 	
 	// Extended ID, 2 Bytes
-	can_frame_rx[0]->can_id = (0x123) | CAN_EFF_FLAG; 
-	can_frame_rx[0]->can_dlc = 2;  
+	can_frame_tx[0]->can_id = (0x123) | CAN_EFF_FLAG; 
+	can_frame_tx[0]->can_dlc = 2;  
 		
     while (1) {
 			
-			CAN_write();
+			//CAN_write();
 			CAN_read();
 			// Delay
 		    vTaskDelay(pdMS_TO_TICKS(1000)); 
